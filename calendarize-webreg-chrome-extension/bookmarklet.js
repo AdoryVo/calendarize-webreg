5ec5a7a577af1fdd658cd8179f61b3f0c72708bf
Adory Vo <business@adoryvo.com>
2023-08-02 21:12:05 -0700

    Update project structure

--- calendarize-webreg-chrome-extension/bookmarklet.js
+++
@@ -1,9 +0,0 @@
-// This code will be executed upon clicking the bookmark
-javascript:(function() {
-	// Fetch the script used to scrape & parse schedule information from the web page 
-	const calendarize = document.createElement('script');
-	calendarize.src = 'https://adoryvo.github.io/calendarize-webreg/calendarize.min.js';
-
-	// Add the script to the current page, causing it to run!
-	document.body.appendChild(calendarize);
-})()
---
+++ privacy-policy.md
@@ -0,0 +1,7 @@
+# Privacy Policy
+
+No data or personal information is collected by **calendarize-webreg**.
+
+#### Contact
+
+If you have any questions or suggestions regarding this privacy policy, do not hesitate to [contact me](https://adoryvo.com/contact).
